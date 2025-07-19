bf1a22ffd18e2febeadae5447a4408cf7764330e
Phoruu <130416055+Phoruu@users.noreply.github.com>
2025-07-20 03:55:01 +0800

    callback functions
    callback functions

--- 2024/1 - web/3 - JS/Drum Kit/index.js
+++ 2024/1 - web/3 - JS/Drum Kit/index.js
@@ -1,7 +1,33 @@
 
 
-document.querySelector("button").addEventListener()
 
-function handleClick(){
-    alert("I got clicked!")
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", 
+        function () {
+            alert("I got clicked! at " + i);
+        }
+    );
 }
+
+
+    
+
+/*
+CALL BACK FUNCTION
+
+
+function add(num1, num2){
+return num1+num2;}
+
+function sub(num1, num2){
+    return num1-num2}
+
+function multiply(num1, num2){
+    return num1*num2}
+
+function div(num1, num2){
+    return num1/num2}
+
+function calculator (num1, num2, operator) {
+    return operator(num1, num2)
+}*/
