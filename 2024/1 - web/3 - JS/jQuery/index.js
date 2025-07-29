f27884eed5d33b898e96beb210cf6910314328a1
Phoruu <130416055+Phoruu@users.noreply.github.com>
2025-07-29 14:44:13 +0800

    [END]> jQuery

--- 2024/1 - web/3 - JS/jQuery/index.js
+++ 2024/1 - web/3 - JS/jQuery/index.js
@@ -1,45 +1,83 @@
 
+// $(document).ready(function () {
 
-// $("h1").click(function(){
-//     $("h1").css("color","purple");
-// })
-
-// $("a").attr("href","https://www.hello.com");
-
-// $('h1').text("Bye");
-
-// $("button").html("<em>Helloe</em>")
-
-//------------------------
-
-// $("button").click(function(){
-//     $("h1").css("color","purple");
-// })
-
-// $(document).keydown(function(event){
-//     $("h1").text(event.key);
-// });
-
-// $("h1").on("mouseover", function() {
 //     $("h1").css("color", "red");
 
-//     setTimeout(function (){
-//         $("h1").css("color", "black");
-//     }, 100);
-// })
+// });
 
-// $("h1").before("<button>New</button>");
+// CSS STLES
+    // console.log($("h1"));
+    // $("button").css("color", "red")
+    // $("h1").addClass("big-title margin-50");
+    // $("h1").removeClass("big-title margin-50");
+    // $("h1").hasClass("margin-50");
 
-// $("button").remove();
 
-// $("button").on("click", function() {
-//     $("h1").slideToggle();
-// })
+// HTML TEXTS
+    // $("h1").text("Bye")
+    // $("button").text("Bye")
+    // $("h1").html("<em>Heylow</em>")
 
-// $("button").on("click", function() {
-//     $("h1").animate({opacity: 0.5});
-// })
+// ATTRIBUTES
+    // $("a").attr("href", "https://www.google.com");
 
-$("button").on("click", function() {
-    $("h1").slideUp().slideDown().animate({opacity:0.5});
-})
+// EVENT LISTENER
+
+    // $("h1").click(function () {
+    //     $("h1").css("color", "red");
+    // })
+
+
+    // MULTIPLE BUTTONS
+    // $("button").click(function () {
+    //     $("h1").css("color", "purple")
+    // })
+
+    // $("body").keydown(function(event){
+    //     console.log(event.key)
+    // })
+
+    // $("body").keydown(function(event){
+    //     $("h1").text(event.key)
+    // })
+
+    // $("h1").on("mouseover", function(){
+    //     $("h1").css("color", "purple");
+    // })
+
+
+// ADD/REMOVE ELEMENTS
+
+    // $("h1").before("<button>Hi</button>")
+    // $("h1").after("<button>Hi</button>")
+    // $("h1").prepend("<button>Hi</button>")
+    // $("h1").append("<button>Hi</button>")
+    // $("button").remove();
+
+// WEBSITE ANIMATION USING JQUERY
+
+    // $("button").on("click", function() {
+    //     $("h1").toggle();
+    // })
+
+    // $("button").on("click", function() {
+    //     $("h1").fadeOut();
+    // })
+
+    // $("button").on("click", function() {
+    //     $("h1").fadeIn();
+    // })
+
+    // $("button").on("click", function() {
+    //     $("h1").fadeToggle();
+    // })
+
+    // $("button").on("click", function() {
+    //     $("h1").slideToggle();
+    // })
+
+    $("button").on("click", function() {
+        $("h1").slideUp().slideDown().animate({
+            margin:0.5
+        });
+    })
