588a7fbd86525075b4285d9965a97f01ba82bdfc
Phoruu <130416055+Phoruu@users.noreply.github.com>
2025-07-31 14:19:43 +0800

    [END]> Section 24: Express.js with Node.js

--- 2024/2 - backend/1 - Node and Express/3.4 Middleware/index4.js
+++ 2024/2 - backend/1 - Node and Express/3.4 Middleware/index4.js
@@ -1,7 +1,32 @@
+import bodyParser from "body-parser";
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+// function bandNameGenerator(req, res, next) {
+//   console.log(req.body);
+//   bandName = req.body["street"] + req.body["pet"];
+//   next();
+// }
+
+// app.use(bandNameGenerator);
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send(`<h1>Band Name is : </h1><br><h2> ${req.body.street} ${req.body.pet} </h2>`)
+})
+
+app.get("/", (req, res) => {
+  console.log(__dirname)
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- 2024/2 - backend/1 - Node and Express/3.5 Secrets Project/index.js
+++ 2024/2 - backend/1 - Node and Express/3.5 Secrets Project/index.js
@@ -1,3 +1,43 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import bodyParser from "body-parser";
+import express from 'express';
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    console.log(__dirname)
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.use(secretCheck);
+
+function secretCheck(req, res, next){
+    console.log(req.body.password);
+    if (req.body.password == 'ILoveProgramming'){
+        next();
+    }
+    else{
+        console.log("Wrong Password!");
+        res.sendFile(__dirname + "/public/index.html");
+    }
+}
+
+app.post("/check", (req, res) => {
+    res.sendFile(__dirname + "/public/secret.html");
+})
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
